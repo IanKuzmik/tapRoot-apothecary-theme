b1034dee6cddf530bec265c5049216173b1fce45
Ian <kuzmik951@gmail.com>
2020-06-28 23:03:42 -0600

    added favicon

--- header.php
+++ header.php
@@ -17,6 +17,7 @@
    <meta charset="<?php bloginfo('charset'); ?>">
    <meta name="viewport" content="width=device-width, initial-scale=1">
    <link rel="profile" href="http://gmpg.org/xfn/11">
+   <link rel="icon" type="png" href="<?php echo get_template_directory_uri().'/img/xochis-favicon-16x16.png'; ?>" > 
    <?php if( is_singular() && pings_open( get_queried_object() ) ): ?>
        <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
    <?php endif; ?>
